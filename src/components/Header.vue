03da7654c9f450ba11719dc0fca84fb824426f30
Ekin Alçar <ekinalcar@users.noreply.github.com>
2020-01-16 12:43:42 +0100

    vue framework
    quiz app

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <b-nav tabs>
+      <b-nav-item disabled>
+        <b>Fancy Quiz App</b>
+      </b-nav-item>
+      <b-nav-item disabled>Counter: 4/10</b-nav-item>
+    </b-nav>
+  </header>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron
+      header="BootstrapVue"
+      lead="Bootstrap v4 Components for Vue.js 2"
+    >
+      <template v-slot:lead>{{ currentQuestion.question }}</template>
+      <hr class="my-4" />
+      <p v-for="answer in answers" :key="answer.id">{{ answer }}</p>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  computed: {
+    answers() {
+      console.log(this.currentQuestion.incorrect_answers);
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  }
+};
+</script>
